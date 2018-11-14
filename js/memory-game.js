ce2e615ec78c1f2120acfd4530680901586a461e
Jddanan <jddanan@gmail.com>
2018-11-15 00:11:24 +0200

    Creating variable in the js

---
+++ js/memory-game.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+var got = {};
+const backCard = "url(../images/card_back.jpg)";
+const card = [{
+    "name": "Lannister",
+    "img": "./images/card1.png",
+},
+{
+    "name": "Stark",
+    "img": "./images/card2.png",
+},
+{
+    "name": "Tyrell",
+    "img": "./images/card3.png",
+},
+{
+    "name": "Targaryen",
+    "img": "./images/card4.png",
+},
+{
+    "name": "Greyjoy",
+    "img": "./images/card5.png",
+},
+{
+    "name": "Bolton",
+    "img": "./images/card6.png",
+},
+{
+    "name": "Baratheon of Storm's End",
+    "img": "./images/card7.png",
+},
+{
+    "name": "Baratheon of King's Landing",
+    "img": "./images/card8.png",
+},
+{
+    "name": "Arryn",
+    "img": "./images/card9.png",
+},
+{
+    "name": "Frey",
+    "img": "./images/card10.png",
+},
+{
+    "name": "Karstark",
+    "img": "./images/card11.png",
+},
+{
+    "name": "Martell",
+    "img": "./images/card12.png",
+},
+];
+
+
+
+});
+
