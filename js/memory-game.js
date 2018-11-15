53888c84a2a154cc088dba06fcc9e002e201182a
Jddanan <jddanan@gmail.com>
2018-11-15 17:03:49 +0200

    adding createboard function

--- js/memory-game.js
+++ js/memory-game.js
@@ -1,58 +1,43 @@
 $(document).ready(function () {
 
-var got = {};
-const backCard = "url(../images/card_back.jpg)";
-const card = [{
-    "name": "Lannister",
-    "img": "./images/card1.png",
-},
-{
-    "name": "Stark",
-    "img": "./images/card2.png",
-},
-{
-    "name": "Tyrell",
-    "img": "./images/card3.png",
-},
-{
-    "name": "Targaryen",
-    "img": "./images/card4.png",
-},
-{
-    "name": "Greyjoy",
-    "img": "./images/card5.png",
-},
-{
-    "name": "Bolton",
-    "img": "./images/card6.png",
-},
-{
-    "name": "Baratheon of Storm's End",
-    "img": "./images/card7.png",
-},
-{
-    "name": "Baratheon of King's Landing",
-    "img": "./images/card8.png",
-},
-{
-    "name": "Arryn",
-    "img": "./images/card9.png",
-},
-{
-    "name": "Frey",
-    "img": "./images/card10.png",
-},
-{
-    "name": "Karstark",
-    "img": "./images/card11.png",
-},
-{
-    "name": "Martell",
-    "img": "./images/card12.png",
-},
-];
-
-
-
 });
 
+// Variable
+var got = {};
+got.difficulty = 4; // Change the number of columns for the difficulty
+got.col = 3;
+got.backCard = "url(./images/card_back.jpg)";
+got.cardArray = [
+    "./images/card1.png",
+    "./images/card2.png",
+    "./images/card3.png",
+    "./images/card4.png",
+    "./images/card5.png",
+    "./images/card6.png",
+    "./images/card7.png",
+    "./images/card8.png",
+    "./images/card9.png",
+    "./images/card10.png",
+    "./images/card11.png",
+    "./images/card12.png",
+];
+
+//Function
+got.start = function () {
+    got.createBoard();
+}
+got.createBoard = function () {
+    for (var i = 0; i < 3; i++) {
+        $("#board").append("<div class ='row'>")
+        for (var j = 0; j < got.difficulty; j++) {
+            $(`.row:nth-child(${i + 1})`).append("<div>")
+            $(`.row div:nth-child(${j + 1})`).addClass(`col-xs-${got.col}`)
+            
+        }
+    }
+    $(`.col-xs-${got.col}`).append("<div class='card'>")
+    $(".card").css({"background":got.backCard,"background-size":"cover"})
+}
+
+
+got.start();
